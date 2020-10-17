f638332ab554a666db0a0078a53ef16e67de37e6
Vu Khanh <nguyenvukhanh0202@gmail.com>
2020-10-17 13:30:21 +0700

    Add Sprite

---
+++ Mario/Sprite.cpp
@@ -0,0 +1,52 @@
+#include "Sprite.h"
+#include "Game.h"
+#include "Debug.h"
+
+/*
+	Sprite
+*/
+Sprite::Sprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex)
+{
+	this->id = id;
+	this->left = left;
+	this->top = top;
+	this->right = right;
+	this->bottom = bottom;
+	this->texture = tex;
+}
+
+void Sprite::Draw(float x, float y, int alpha)
+{
+	Game * game = Game::GetInstance();
+	game->Draw(x, y, texture, left, top, right, bottom, alpha);
+}
+
+/*
+	List Sprites
+*/
+
+/*
+	Using Singleton Design Pattern to init list sprites
+*/
+Sprites * Sprites::__instance = NULL;
+
+Sprites *Sprites::GetInstance()
+{
+	if (__instance == NULL) __instance = new Sprites();
+	return __instance;
+}
+
+
+/*
+	Add Sprite to ListSprite to manage
+*/
+void Sprites::Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex)
+{
+	LPSprite s = new Sprite(id, left, top, right, bottom, tex);
+	listSprites[id] = s;
+}
+
+LPSprite Sprites::Get(int id)
+{
+	return listSprites[id];
+}
---
+++ Mario/Sprite.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <Windows.h>
+#include <d3dx9.h>
+#include <unordered_map>
+
+using namespace std;
+
+typedef Sprite * LPSprite;
+
+class Sprite
+{
+	int id;				// Sprite ID in the sprite database
+
+	int left;
+	int top;
+	int right;
+	int bottom;
+
+	LPDIRECT3DTEXTURE9 texture;
+public:
+	Sprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
+
+	void Draw(float x, float y, int alpha = 255);
+};
+
+/*
+	Manage sprite database
+*/
+class Sprites
+{
+	static Sprites * __instance;
+
+	unordered_map<int, LPSprite> listSprites;
+
+public:
+	void Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
+	LPSprite Get(int id);
+	LPSprite &operator[](int id) { return listSprites[id]; }
+
+	static Sprites * GetInstance();
+};
+
+
